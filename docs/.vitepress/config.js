76be4e413bec564ec0b309a75fdf0cec9ee6be2c
mao <936137804@qq.com>
2023-01-30 11:42:23 +0800

    home page

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,12 +2,49 @@
  * @Author: mao 936137804@qq.com
  * @Date: 2023-01-30 09:04:15
  * @LastEditors: mao 936137804@qq.com
- * @LastEditTime: 2023-01-30 09:32:08
+ * @LastEditTime: 2023-01-30 11:40:36
  * @FilePath: /myBlog/docs/.vitepress/config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 export default {
-    title: 'VitePress',
-    description: 'Just playing around.',
+    title: 'BestMao',
     base: '/MyBlog/',
+    appearance: true,
+    themeConfig: {
+        logo: '/img/icon.png',
+        nav: [
+            {
+                text: '框架学习',
+                items: [
+                    { text: 'Vue', link: '/item-1' },
+                    { text: 'React', link: '/item-2' },
+                ]
+            },
+            {
+                text: '工作积累',
+                items: [
+                    { text: 'nodejs', link: '/item-1' },
+                    { text: 'Typescript', link: '/item-2' },
+                    { text: '项目综合', link: '/item-2' },
+                    { text: 'webgl', link: '/item-2' },
+                    { text: 'gis', link: '/item-2' },
+                    { text: '计算机网络', link: '/item-2' },
+                ]
+            },
+            { text: '算法', link: '/guide' },
+            { text: '面试资料', link: '/guide' },
+        ],
+        sidebar: [
+            {
+                text: 'Guide',
+                items: [
+                    { text: 'Introduction', link: '/introduction' },
+                    { text: 'Getting Started', link: '/getting-started' },
+                ]
+            }
+        ],
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+        ]
+    }
 }
--- docs/index.md
+++ docs/index.md
@@ -2,17 +2,27 @@
 layout: home
 
 hero:
-  name: VitePress
-  text: Vite & Vue powered static site generator.
-  tagline: Lorem ipsum...
-  #   image:
-  #     src: /logo.png
-  #     alt: VitePress
+  name: Welcome Blog
+  text: 一个前端仔的                    日常记录
+  tagline: 没有更多...
+  image:
+    src: /img/icon.png
+    alt: VitePress
   actions:
     - theme: brand
-      text: Get Started
+      text: 进来看看
     #   link: /guide/what-is-vitepress
     - theme: alt
-      text: View on GitHub
+      text: 我的GitHub
       link: https://github.com/vuejs/vitepress
+features:
+  - icon: 🛠️
+    title: 框架学习
+    details: 将研究技术、阅读源码的过程记录下来，不管作为分享，还是自己以后回顾，都是一种很不错的方式
+  - icon: 📖
+    title: 工作积累
+    details: 把学习整理的知识记录下来，对知识进一步理解，系统性学习
+  - icon: 🧮
+    title: 算法
+    details: 前端掌握的基本算法与数据结构，经典例题记录
 ---
