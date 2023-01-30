f681f4c7826668f43c6106906020e361cf098014
mao <936137804@qq.com>
2023-01-30 09:32:18 +0800

    test

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,12 +2,12 @@
  * @Author: mao 936137804@qq.com
  * @Date: 2023-01-30 09:04:15
  * @LastEditors: mao 936137804@qq.com
- * @LastEditTime: 2023-01-30 09:26:02
+ * @LastEditTime: 2023-01-30 09:32:08
  * @FilePath: /myBlog/docs/.vitepress/config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 export default {
     title: 'VitePress',
     description: 'Just playing around.',
-    base: '/',
+    base: '/MyBlog/',
 }
