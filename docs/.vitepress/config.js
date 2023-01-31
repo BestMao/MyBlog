f077efe3b70db5649cd4f1ca8f2f9e60049f6e2b
mao <936137804@qq.com>
2023-01-31 17:12:55 +0800

    vue

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,7 @@
  * @Author: mao 936137804@qq.com
  * @Date: 2023-01-30 09:04:15
  * @LastEditors: mao 936137804@qq.com
- * @LastEditTime: 2023-01-30 16:17:18
+ * @LastEditTime: 2023-01-31 17:11:02
  * @FilePath: /myBlog/docs/.vitepress/config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -24,7 +24,7 @@ export default {
                 text: '框架学习',
                 items: [
                     { text: 'React', link: '/frame/react/React 进阶实践指南(笔记)' },
-                    { text: 'Vue', link: '/item-2' },
+                    { text: 'Vue', link: '/frame/vue/准备工作（源码）' },
                 ]
             },
             {
@@ -51,6 +51,12 @@ export default {
                     text: "react整体流程",
                     link: "/frame/react/react整体流程",
                 },
+            ],
+            "/frame/vue": [
+                {
+                    text: '准备工作（源码）',
+                    link: "/frame/vue/准备工作（源码）"
+                }
             ]
         },
         socialLinks: [
