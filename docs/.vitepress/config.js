661261eb21d81148e511b92f1becf03b4da93346
mao <936137804@qq.com>
2023-02-03 17:28:10 +0800

    vue

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,7 @@
  * @Author: mao 936137804@qq.com
  * @Date: 2023-01-30 09:04:15
  * @LastEditors: mao 936137804@qq.com
- * @LastEditTime: 2023-02-01 17:09:14
+ * @LastEditTime: 2023-02-03 17:27:20
  * @FilePath: /myBlog/docs/.vitepress/config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -60,6 +60,10 @@ export default {
                 {
                     text: '组件化（源码）',
                     link: "/frame/vue/组件化（源码）"
+                },
+                {
+                    text: '数据驱动（源码）',
+                    link: "/frame/vue/数据驱动（源码）"
                 }
             ]
         },
